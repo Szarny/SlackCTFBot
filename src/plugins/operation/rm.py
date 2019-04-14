a8cf0f9760265d2ec28645aa4f6006cbf5058b11
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2019-04-15 01:14:56 +0900

    :+1: Implement rm function

---
+++ src/plugins/operation/rm.py
@@ -0,0 +1,25 @@
+import pickle
+
+def rm(rm_target_id):
+    ctflist = []
+    with open("db/ctfinfo.pickle", "rb") as f:
+        ctflist = pickle.load(f)
+
+    for idx, ctf in enumerate(ctflist):
+        id_ = ctf["id_"]
+        title = ctf["title"]
+        date = ctf["date"]
+
+        if id_ == rm_target_id:
+
+            removed_ctflist = ctflist[:idx] + ctflist[idx+1:]
+            with open("db/ctfinfo.pickle", "wb") as f:
+                pickle.dump(removed_ctflist, f)
+
+            return "*Delete* `{id_}: {title} ({date})`".format(
+                id_=id_,
+                title=title,
+                date=date
+            )
+    
+    return "ID not found :cry:"
