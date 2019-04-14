984f68a032fc8da11fd7a580d57dfc344496893e
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2019-04-15 00:59:30 +0900

    :+1: Add ls function

---
+++ src/plugins/operation/ls.py
@@ -0,0 +1,17 @@
+import pickle
+
+def ls():
+    ctflist = []
+    with open("db/ctfinfo.pickle", "rb") as f:
+        ctflist = pickle.load(f)
+
+    reply_message = ""
+    message_tmpl = "- {id_}: {title} ({date})\n"
+    for ctf in ctflist:
+        reply_message += message_tmpl.format(
+            id_=ctf["id_"],
+            title=ctf["title"],
+            date=ctf["date"]
+        )
+
+    return reply_message
