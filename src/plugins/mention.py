d22702c14e34449218f0c33f683582dbc2ad1c8a
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2019-04-15 00:21:30 +0900

    :+1: Add reply function

---
+++ src/plugins/mention.py
@@ -0,0 +1,27 @@
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+from slackbot.bot import default_reply
+
+
+@respond_to("ctftime")
+def mention_ctf_time(message):
+    message.react("flag")
+    message.reply("ctftime")
+
+
+@respond_to("save")
+def mention_help(message):
+    message.react("pencil")
+    message.reply("save")
+
+
+@respond_to("ls")
+def mention_ls(message):
+    message.react("ok_hand")
+    message.reply("ls")
+
+
+@respond_to("help")
+def mention_help(message):
+    message.react("question")
+    message.reply("help")
