d025553a9641f964ddf87bccf5f8b505880db237
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2019-04-15 00:09:55 +0900

    :+1: Add main script and settings.

---
+++ src/run.py
@@ -0,0 +1,11 @@
+from slackbot.bot import Bot as SlackBot
+
+from output import output
+
+def main():
+    slackbot = SlackBot()
+    slackbot.run()
+
+if __name__ == "__main__":
+    output.info("Slack Bot is activated.")
+    main()
---
+++ src/slackbot_settings.py
@@ -0,0 +1,7 @@
+import os
+
+API_TOKEN = os.getenv("ctf_slack_bot_api_token")
+
+DEFAULT_REPLY = "I can't understand your message... :cry:"
+
+PLUGINS = ['plugins']
