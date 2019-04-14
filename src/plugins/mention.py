84b7235f46187e77b8dfc829788236aa2021fe52
Tsubasa Umeuchi <tsubasa.umeuchi@gmail.com>
2019-04-15 01:29:14 +0900

    :+1: Implement help function

--- src/plugins/mention.py
+++ src/plugins/mention.py
@@ -5,21 +5,22 @@ from slackbot.bot import default_reply
 from .operation.save import save
 from .operation.ls import ls
 from .operation.rm import rm
+from .operation.help import help_
 
 
 @respond_to("ctftime")
 def mention_ctf_time(message):
     message.react("checkered_flag")
+
     message.reply("ctftime")
 
 
 @respond_to("save")
 def mention_help(message):
-    message.react("pencil")
+    message.react("pencil2")
 
     title, date = message.body["text"].split()[1].split("|")
     result = save(title, date)
-
     message.reply(result)
 
 
@@ -29,20 +30,20 @@ def mention_help(message):
 
     rm_target_id = message.body["text"].split()[1]
     result = rm(rm_target_id)
-
     message.send(result)
 
 
 @respond_to("ls")
 def mention_ls(message):
-    message.react("ok_hand")
+    message.react("pencil")
 
     result = ls()
-
     message.send(result)
 
 
 @respond_to("help")
 def mention_help(message):
     message.react("question")
-    message.reply("help")
+
+    result = help_()
+    message.send(result)
---
+++ src/plugins/operation/help.py
@@ -0,0 +1,18 @@
+def help_():
+    return """:thinking_face: *HOW TO USE slack_ctf_bot* :thinking_face:
+
+    usage: `@slack_ctf_bot <command> (<args>)`
+
+    *Command*
+    `ctftime` : Show CTF events announced on CTFTime. :checkered_flag:
+    `ls` : Show CTF events saved by members. :pencil:
+    `save <title>|<date>` : Save CTF events. :pencil2:
+    `rm <id>` : Remove CTF events from list that shown in ls command. :x:
+    `help` : Show help message. :question:
+
+    *Example*
+    `save FooBarCTF|20190101-20191231`
+    `rm abcde`
+    
+    *Note*
+    Repository: https://github.com/Szarny/SlackCTFBot"""
